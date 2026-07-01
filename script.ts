0a70ada120e8e6ada2f65ab9e002fba02555fd58
restya <restya.astari@gmail.com>
2026-07-01 10:46:49 +0700

    refactor: remove legacy presentation sync tools
    Clean up unused environment variables and script files related to the presentation sync process.

--- script.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-fetch('https://docs.google.com/presentation/d/1rPOKgqnbRrUUwizA-j1VCihSrQ0bdtmGPtP7Qkdbji4/export/txt')
-  .then(res => res.text())
-  .then(text => {
-    fs.writeFileSync('presentation.txt', text);
-    console.log('Downloaded');
-  })
-  .catch(console.error);
